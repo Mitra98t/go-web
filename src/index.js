7d4a0702ac45672b0e5221e14dcb344d84d50e21
Mitra98t <personalmailfm98@gmail.com>
2024-07-26 12:19:00 +0200

    new game ui

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <NotFound />,
+    // errorElement: <NotFound />,
   },
   {
     path: "/home",
